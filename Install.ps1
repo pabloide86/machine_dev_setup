5acf9b576e88ef7eacf99a3243755843f6e19506
pablo <pabloide86@hotmail.com>
2018-05-21 21:01:00 -0300

    Initial commit

---
+++ Install.ps1
@@ -0,0 +1,76 @@
+# Description: Boxstarter Script  
+# Author: Microsoft
+# Common dev settings for desktop app development
+
+Disable-UAC
+
+#--- Windows Features ---
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions
+
+#--- File Explorer Settings ---
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneExpandToCurrentFolder -Value 1
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name NavPaneShowAllFolders -Value 1
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Value 1
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name MMTaskbarMode -Value 2
+
+
+# Install chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#--- Windows Subsystems/Features ---
+choco install -y Microsoft-Hyper-V-All -source windowsFeatures
+choco install -y Microsoft-Windows-Subsystem-Linux -source windowsfeatures
+
+#--- Tools ---
+#choco install -y visualstudio2017community  # See this for install args: https://chocolatey.org/packages/VisualStudio2017Community
+
+# TODO: should pass install args to VS2017 to install additional options 
+
+
+#--- Browsers ---
+choco install -y googlechrome
+choco install -y firefox
+
+#--- Fonts ---
+choco install -y inconsolata
+# choco install -y ubuntu.font
+
+#--- Tools ---
+choco install -y sysinternals
+choco install -y winrar
+choco install -y git -params '"/GitAndUnixToolsOnPath /WindowsTerminal"'
+choco install -y poshgit
+choco install -y curl
+choco install -y freecommander-xe.install
+choco install -y winscp
+choco install -y greenshot
+choco install -y lessmsi
+choco install -y tortoisesvn
+choco install -y baretail
+choco install -y postman
+
+#--- Console Emulators ---
+choco install -y cmder
+choco install -y hyper
+
+#--- Code Editors ---
+choco install -y vscode
+choco install -y sublimetext3
+choco install -y neovim
+
+#--- Nodejs ---
+choco install -y nodejs-lts
+choco install -y yarn
+choco install -y mongodb
+
+#--- Golang ---
+choco install -y golang
+
+#--- dotnet ---
+choco install -y dotnetcore-sdk
+choco install -y ilspy
+choco install -y linqpad
+
+Enable-UAC
+Enable-MicrosoftUpdate
+Install-WindowsUpdate -acceptEula
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # windows_dev_setup
 Scripts for setting up a new Windows dev machine
+
+Inspired by:
+- https://github.com/Microsoft/windows-dev-box-setup-scripts
+- https://github.com/felixrieseberg/windows-development-environment
