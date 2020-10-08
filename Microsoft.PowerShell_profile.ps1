a8eda0e3121eec81004fc709f0e3b2fbc00f4b62
pabloide86 <pablo.rotondo@gmail.com>
2020-10-08 09:48:24 -0300

    Update Microsoft.PowerShell_profile.ps1
    Update oh-my-posh
    Add git shortcuts

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,14 +1,74 @@
 # Copy to C:\Users\$USER$\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
 
 Import-Module posh-git
-Import-Module oh-my-posh
-Set-Theme Agnoster
+
+# Update path to oh-my-posh version and username
+set-poshprompt -Theme "C:\Users\$USER$\Documents\PowerShell\Modules\oh-my-posh\<version>\themes\agnoster.json"
+
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Autocompletion for arrow keys
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+# Only of PSCore - Set-Clipboard alias scb
+function scb{
+    param(
+       [parameter(position=0,mandatory=$true,ValueFromPipeline=$true)]$Text
+    )
+    begin{
+        $data = [system.text.stringbuilder]::new()
+    }
+
+    process{
+        if ($text){
+            [void]$data.appendline($text)
+        }
+    }
+
+    end{
+        if ($data){
+            $data.tostring().trimend([environment]::newline) + [convert]::tochar(0) | clip.exe
+        }
+    }
+}
 
 # Useful shortcuts for jumping dirs
 function godesktop { cd ~\Desktop }
-function godocs { cd ~\Documents }
-function godownloads { cd ~\Downloads }
+function godev { cd ~\Development }
+function lss {
+    ls | sort @{Expression="mode"; Descending=$true},@{Expression="name"; Descending=$false}
+}
 
 # Useful commands
-function currdir { scb (pwd).Path }
-function killem($imageName) { taskkill /f /im $imageName /t }
+function currdir { (pwd).Path | scb }
+function shrug { "¯\_(ツ)_/¯" | scb }
+function fliptable { "(┛ಠ_ಠ)┛彡┻━┻" | scb }
+function killproc($imageName) { taskkill /f /im $imageName /t }
+
+# Git shortcuts
+function glog() { git log --oneline -10 }
+function gs() { git status }
+function gds() { git diff --staged }
+function gca(msg) { git commit -m -a msg }
+function grev() { git reset --hard HEAD; git clean -fdx; }
+
+# Initialize a git directory with a readme
+function ginit($name, $description) {
+  if ( -not (Test-Path $name)) {
+    mkdir $name;
+    cd $name;
+    git init;
+    $description > Readme.md;
+    git add Readme.md;
+    git commit -m "Initial commit";
+  } else {
+      Write-Host "Folder already exists"
+      cd $name;
+  }
+}
+
+# Android Development
+function startavd() { C:\Users\Pablo\AppData\Local\Android\Sdk\emulator\emulator.exe -avd "Pixel_2_API_26" }
+
